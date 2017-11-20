14f7d832bb851c9b12e5209e5540a292dc4cd4e3
jessamynsmith <jessamyn.smith@gmail.com>
2017-11-19 21:24:26 -0700

    Added a README and an es6 version of the code.

---
+++ README.md
@@ -0,0 +1,23 @@
+# paged-api-request
+
+This is an example of making an API call recursively until there are no more pages to retrieve.
+
+
+Like my work? Tip me! https://www.paypal.me/jessamynsmith
+
+
+### Development
+
+Ensure you have node installed (I recommend using homebrew on OSX), then use npm to install JavaScript dependencies:
+
+    npm install
+
+Run the application:
+
+    node main.js
+
+Or:
+
+    node main_es6.js
+
+You can edit the API url as desired in main.js or main_es6.js.
--- main_es6.js
+++ main_es6.js
@@ -3,17 +3,17 @@ var request = require('request');
 var url = 'https://underquoted.herokuapp.com/api/v2/quotations/?limit=20&offset=1600';
 
 
-function callPagedApi(url, previousData) {
+callPagedApi = (url, previousData) => {
   var deferred = Promise.defer();
 
-  request(url, function (error, response, body) {
+  request(url, (error, response, body) => {
     if (error) {
       deferred.reject(error);
     }
     var data = JSON.parse(body);
     var allData = previousData.concat(data.results);
     if (data.next) {
-      callPagedApi(data.next, allData).then(function (data) {
+      callPagedApi(data.next, allData).then((data) => {
         deferred.resolve(data);
       });
     } else {
@@ -22,8 +22,8 @@ function callPagedApi(url, previousData) {
   });
 
   return deferred.promise;
-}
+};
 
-callPagedApi(url, []).then(function (data) {
+callPagedApi(url, []).then((data) => {
   console.log(data.length);
 });
